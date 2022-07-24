43005e92f2881dc9b8edac05e8d32265fcb2ad4e
ARBONA Julien <julien.arbona@outlook.fr>
2022-07-24 23:39:48 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector('.nav-container').classList.add('scrolled');
+    document.querySelector('.nav-container ul li button').classList.remove('dark');
+    document.querySelector('.nav-container ul li button').classList.add('light');
+} else {
+    document.querySelector('.nav-container').classList.remove('scrolled');
+    document.querySelector('.nav-container ul li button').classList.remove('light');
+    document.querySelector('.nav-container ul li button').classList.add('dark');
+  }
+}
+
+document.querySelector('input#btn_menuBurger').addEventListener('change', (e) => {
+  if(e.currentTarget.checked) {
+    // console.log('Checked !');
+    document.querySelector('label[for=' + e.currentTarget.id + '] img').src = '/src/close-icon.svg';
+  } else {
+    // console.log('Not Checked !');
+    document.querySelector('label[for=' + e.currentTarget.id + '] img').src = '/src/burgerMenu-icon.svg';
+
+  }
+});
